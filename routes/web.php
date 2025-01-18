4df330c638f5c6890a405f386401dffada0ccc31
Subham Akhuli <subhamakhuli7586@gmail.com>
2025-01-18 12:11:18 +0530

    Add user routes to handle user information and views

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// Import the UserController
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +11,11 @@ Route::get('/', function () {
 Route::get('/home', function() {
     return view('home');
 });
+
+Route::get('user', [UserController::class, 'user']);
+
+// This route will accept a parameter and pass it to the controller
+// Route::get('user/{name}', [UserController::class, 'userName']);
+
+// This route will accept a parameter and pass it to the controller
+Route::get('user/{name}', [UserController::class, 'view']);
